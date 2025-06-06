e2894b1fe24ed08ecdb017bc4043db322c13ca99
bbbbbb280 <nissanspamty@gmail.com>
2025-06-06 13:09:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// De carousel werkt automatisch via CSS animatie.
+// Wil je handmatige knoppen? Hier een simpel voorbeeld voor toekomstige uitbreiding:
+
+// const slide = document.querySelector('.carousel-slide');
+// let counter = 0;
+// function nextSlide() {
+//   counter++;
+//   slide.style.transform = `translateX(-${counter * 300}px)`;
+// }
